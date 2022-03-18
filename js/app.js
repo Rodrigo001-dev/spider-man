ebc79649b67420332b45ef344535dff042d84112
Rodrigo <rodrigorael53@gmail.com>
2022-03-18 18:09:05 -0300

    criando a tela de loading

---
+++ js/app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // timeline
+  let tl = new TimelineMax();
+
+  // fromTo é uma função onde ele vai de algum lugar para outro lugar
+  tl.fromTo()
+
+});
